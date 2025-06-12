1cee35d0d4ae7d9b85cb1d56df7daabcef427da4
Domaakshithareddy <akshithareddydoma7@gmail.com>
2025-06-12 19:55:25 +0530

    Update train

--- GPU.py
+++ GPU.py
@@ -1,2 +1,4 @@
 import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+print("TensorFlow version:", tf.__version__)
+print("GPU Available:", tf.config.list_physical_devices('GPU'))
